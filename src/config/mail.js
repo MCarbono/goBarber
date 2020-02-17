b3c7967b3744b6e6aa52d470118a8a71b92c77bd
Marcelo <carbono.marcelo@gmail.com>
2020-02-17 00:17:23 -0300

    nodemailer configuration

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '6c69101a5c74a3',
+    pass: 'ae0012ff6691db',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
