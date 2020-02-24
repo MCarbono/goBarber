dcec55b1235f963a3e99f8736df64e977c7bccc1
Marcelo <carbono.marcelo@gmail.com>
2020-02-24 17:34:40 -0300

    finished backend

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '6c69101a5c74a3',
-    pass: 'ae0012ff6691db',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
